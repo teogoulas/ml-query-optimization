d9a49c968d2ca38140d60aae9d52ca5d42c2bf25
Thodoris Goulas <t.goulas@paymentcomponents.com>
2021-12-30 23:43:57 +0200

    Added sql query parser.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import psqlparse
 
 from models.database import Database
 from utils.parser import parse_json, flatten_tree
@@ -10,10 +11,12 @@ def main():
     files = os.listdir(os.path.join(cwd, *dataset.split("/")))
 
     db = Database(collect_db_info=True)
-    cursor = db.conn.cursor()
     for file in files:
         f = open(dataset + "/" + file, "r")
         query = f.read()
+
+        statements = psqlparse.parse('SELECT * from mytable')
+        used_tables = statements[0].tables()
         rows = db.explain_query(query)
         encoded_node = parse_json(rows, db)
         print(flatten_tree(encoded_node))
